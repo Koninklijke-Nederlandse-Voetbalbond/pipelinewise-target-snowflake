e5f2b5f34bee8bed659779bbcbf876214721ec2a
Rob Winters <wintersrd@gmail.com>
2024-08-29 14:28:26 +0200

    Add jsonschema

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(name="pipelinewise-target-snowflake",
           'joblib==1.2.0',
           'numpy==1.26.4',
           'boto3==1.28.20',
+          'jsonschema==3.2.0'
       ],
       extras_require={
           "test": [
